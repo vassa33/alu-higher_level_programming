b283d71c3ed0b651d1ef18f5600bf761aeedd57c
vassa33 <peggyongoma@gmail.com>
2022-06-29 02:31:24 -0700

    ByteCode -> Python #1

--- python-hello_world/102-magic_calculation.py
+++ python-hello_world/102-magic_calculation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-    else:
-        return (sub(a, b))
+    if b > a:
+        return (b)
+
+    if a > b:
+        return (a)
+
+    return (a * b)
