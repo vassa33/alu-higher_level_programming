1aac0ddb0bc7362edaf7203e267249fa77604860
vassa33 <peggyongoma@gmail.com>
2022-07-13 06:53:28 -0700

    Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     for k, v in a_dictionary.items():
-        if v == False:
+        if v is False:
             score = 'None'
         else:
             score = max(a_dictionary, key=a_dictionary.get)
