5f736979994c387d82689857ef6d27e9a1d6ac21
Vanessa Peggy Otieno <61325877+vassa33@users.noreply.github.com>
2022-07-27 15:48:18 +0300

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-""" Creates class MyList """
+""" module 1-my_list contains the class MyList """
 
 
 class MyList(list):
     """ defines MyList class """
+
+    def __init__(self):
+        """ initializes new MyList object """
+        pass
+
     def print_sorted(self):
         """ prints sorted list """
         print(sorted(self))
