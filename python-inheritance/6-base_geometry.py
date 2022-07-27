61a17f7a5c7d0925376f15c7a5ad0543e37e3ea1
Vanessa Peggy Otieno <61325877+vassa33@users.noreply.github.com>
2022-07-27 16:19:53 +0300

    Update 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""contains class BaseGeometry"""
+
+
+class BaseGeometry:
+    """defines class BaseGeometry"""
+    
+    def area(self):
+        """raises exception"""
+        raise Exception('area() is not implememented')
