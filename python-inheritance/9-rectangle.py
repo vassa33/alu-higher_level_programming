e2543f3c51723e6512f985881bf6e17a98c07586
Vanessa Peggy Otieno <61325877+vassa33@users.noreply.github.com>
2022-07-27 16:41:55 +0300

    Update 9-rectangle.py

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" module 8-rectangle contains subclass Rectangle
+which inherits from class BaseGeometry """
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ defines class Rectangle """
+
+    def __init__(self, width, height):
+        """ initializes empty Rectangle and
+        validates width and height as positive integers"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ returns area of Rectangle """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ returns unofficial string representation of Rectangle """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
