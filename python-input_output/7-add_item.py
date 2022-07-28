e6f08b740a685c96afce968ed9f09b7e3f4a4fc2
Vanessa Peggy Otieno <61325877+vassa33@users.noreply.github.com>
2022-07-28 18:07:16 +0300

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,9 +7,13 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 try:
+    """testing for error"""
     my_list = load_from_json_file(filename)
 except:
+    """handling any error encountered"""
     my_list = []
 for arg in argv[1:]:
+    """appending arguments to the list,
+    and saving it to a JSON file"""
     my_list.append(arg)
 save_to_json_file(my_list, filename)
