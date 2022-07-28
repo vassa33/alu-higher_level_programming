3b12e8db3f7977e7b8997ac8c9712b302e399b97
Vanessa Peggy Otieno <61325877+vassa33@users.noreply.github.com>
2022-07-28 17:49:03 +0300

    Update 3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" contains the to_json_string function """
+import json
+
+
+def to_json_string(my_obj):
+    """ returns JSON representation of an object """
+    return json.dumps(my_obj)
