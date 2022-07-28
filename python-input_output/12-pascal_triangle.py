ea48865945ef4abdde7e78f43846b5b94fd545e2
Vanessa Peggy Otieno <61325877+vassa33@users.noreply.github.com>
2022-07-28 18:46:18 +0300

    Update 12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" contains the pascal_triangle function """
+
+
+def pascal_triangle(n):
+    """ returns the pascal triangle of n """
+    if n <= 0:
+        return []
+    p_t = [[] for x in range(n)]
+    p_t[0] = [1]
+    if n > 1:
+        p_t[1] = [1, 1]
+    if n > 2:
+        for i in range(2, n):
+            p_t[i].append(1)
+            for j in range(i - 1):
+                p_t[i].append(p_t[i - 1][j] + p_t[i - 1][j + 1])
+            p_t[i].append(1)
+    return p_t
