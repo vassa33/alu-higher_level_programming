447ce90960caad9917849e70d96139b6cc5e20c6
Vanessa Peggy Otieno <61325877+vassa33@users.noreply.github.com>
2022-07-28 18:33:56 +0300

    Update 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" contains the class Student """
+
+
+class Student:
+    """ defines class Student """
+
+    def __init__(self, first_name, last_name, age):
+        """ initalizes new Student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves dictionary representation of a Student instance """
+        if attrs is None or type(attrs) is not list:
+            return self.__dict__
+        for att in attrs:
+            if type(att) is not str:
+                return self.__dict__
+        return {x: self.__dict__[x] for x in self.__dict__ if x in attrs}
