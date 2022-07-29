903431d9901086aff29b2ff91f58a6a92df2f203
Vanessa Peggy Otieno <61325877+vassa33@users.noreply.github.com>
2022-07-29 16:05:41 +0300

    Update 6-peak.py

--- python-network_0/6-peak.py
+++ python-network_0/6-peak.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
-''' contains function find_peak '''
+""" Function that finds a peak in a list of unsorted integers. """
 
 
 def find_peak(list_of_integers):
-    ''' finds a peak in a list of unsorted integers '''
-    if not list_of_integers:
-        return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    left = 0
-    right = len(list_of_integers) - 1
-    return peak_find(list_of_integers, left, right)
+    """ Sort the list """
 
-
-def peak_find(list, left, right):
-    ''' finds peak recursively using binary search '''
-    if left == right:
-        return(list[right])
-    mid = (left + right) // 2
-    if list[mid] > list[mid + 1]:
-        return peak_find(list, left, mid)
-    return peak_find(list, mid + 1, right)
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
