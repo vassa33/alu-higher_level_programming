a61c56e4fe385a887901b7d9811b184e4e420014
vassa33 <peggyongoma@gmail.com>
2022-08-02 14:35:32 -0700

    Temperatures #2

---
+++ SQL_introduction/103-max_state.sql
@@ -0,0 +1,3 @@
+-- script that shows the maximum temperature of each state --
+-- (ordered by state name) --
+SELECT `state`, MAX(`value`) 'max_temp' FROM `temperatures` GROUP BY `state` ORDER BY `state` ASC;
