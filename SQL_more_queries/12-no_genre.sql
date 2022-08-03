e4e9608d6b41e2ffb097b0731ed1b9e696ab7955
vassa33 <peggyongoma@gmail.com>
2022-08-03 10:06:21 -0700

    Number of shows by genre

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,2 @@
--- lists all shows contained in hbtn_0d_tvshows
--- if a show does not have a genre linked, display NULL
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+-- List only movies without a genre
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
