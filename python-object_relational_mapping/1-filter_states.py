0bdee35fd205c524e63e2ed21f31396a895761d2
Vanessa Peggy Otieno <61325877+vassa33@users.noreply.github.com>
2022-08-22 13:33:17 +0300

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-""" module list states
-from database"""
+''' lists all states from database hbtn_0e_0_usa that start with 'N' '''
+
 
 if __name__ == "__main__":
-    import MySQLdb
     from sys import argv
-    # port and host are default local and 3306
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
-    result = cur.fetchall()
-    # display elements with N
-    # only by taking comparing their first letter in tuple
-    for i in result:
-        if i[1][0] == 'N':
-            print(i)
-    # close cursor and db
+    cur.execute("SELECT * from states\
+                WHERE name LIKE 'N%' COLLATE latin1_general_cs\
+                ORDER BY states.id")
+    state_list = cur.fetchall()
+    for state in state_list:
+        print(state)
     cur.close()
     db.close()
